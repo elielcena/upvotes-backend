8775f146078afc5fed123d07d3c1c8e9f0c46416
elielcena <45287391+elielcena@users.noreply.github.com>
2020-12-17 10:51:56 -0300

    1 - Criar Projeto Spring

---
+++ src/main/java/br/com/segware/PostUpvotesApplication.java
@@ -0,0 +1,13 @@
+package br.com.segware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostUpvotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostUpvotesApplication.class, args);
+	}
+
+}

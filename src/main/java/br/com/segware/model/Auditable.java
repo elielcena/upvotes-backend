16a4557f3078d69d09649e73397121888f61a11e
elielcena <45287391+elielcena@users.noreply.github.com>
2020-12-17 11:41:33 -0300

    3 - Criar Campos de Auditoria

---
+++ src/main/java/br/com/segware/model/Auditable.java
@@ -0,0 +1,30 @@
+package br.com.segware.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter(AccessLevel.PROTECTED)
+@Setter(AccessLevel.PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+	@CreatedDate
+	@Column(name = "datagravacao")
+	private Date datagravacao;
+
+	@LastModifiedDate
+	@Column(name = "dataalteracao")
+	private Date dataalteracao;
+}
